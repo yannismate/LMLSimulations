99708cac97cca1769ce480366596e6ebc974546a
Richard <strunk876@gmail.com>
2025-01-13 13:58:38 +0100

    Fix stuff

--- src/main/java/de/tum/logistics/Main.java
+++ src/main/java/de/tum/logistics/Main.java
@@ -1,34 +1,22 @@
 package de.tum.logistics;
 
-import org.eclipse.sumo.libsumo.Simulation;
-import org.eclipse.sumo.libsumo.StringVector;
+import org.eclipse.sumo.libtraci.Simulation;
+import org.eclipse.sumo.libtraci.StringVector;
 
 import java.io.File;
 import java.util.HashSet;
 import java.util.Set;
 
 public class Main {
-  private static Set<String> libraries = new HashSet<>();
-  static {
-    libraries.add("libtracijni");
-    libraries.add("iconv-2");
-    libraries.add("intl-8");
-    libraries.add("proj_9_0");
-    libraries.add("libsumojni");
-    libraries.add("libtracijni");
-  }
   private static File resourceFolder = new File("src/main/resources");
   public static void main(String[] args) {
-    for (String libName : libraries) {
-      System.loadLibrary(libName);
-    }
-
+    System.loadLibrary("libtracijni");
     File netFile = new File(resourceFolder, "osm.net.xml.gz");
-
-    Simulation.start(new StringVector(new String[]{"sumo", "-n", netFile.getAbsolutePath()}));
+    Simulation.start(new StringVector(new String[]{"sumo-gui", "-n", netFile.getAbsolutePath()}));
     for (int i = 0; i < 5; i++) {
       Simulation.step();
     }
     Simulation.close();
+    Simulation.subscribe();
   }
 }
