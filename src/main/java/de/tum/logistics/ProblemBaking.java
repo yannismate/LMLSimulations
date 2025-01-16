25b517ac5461d2c0aede65c85caf8dfb605fe43f
Yannis Matezki <git@yannismate.de>
2025-01-16 11:38:43 +0100

    Add multi-carrier routing

--- src/main/java/de/tum/logistics/ProblemBaking.java
+++ src/main/java/de/tum/logistics/ProblemBaking.java
@@ -1,8 +1,10 @@
 package de.tum.logistics;
 
 import java.io.File;
+import java.util.HashSet;
 import java.util.List;
 
+import me.tongfei.progressbar.ProgressBar;
 import org.eclipse.sumo.libtraci.Simulation;
 import org.eclipse.sumo.libtraci.StringVector;
 
@@ -20,11 +22,40 @@ public class ProblemBaking {
     VehicleType.copy("DEFAULT_VEHTYPE", "passenger");
 
     List<OsmNode> possibleLocations = GeoJsonParser.parseOsmNodes(new File(Main.RESOURCE_FOLDER, "addresses.json"));
-    TruckDeliveryProblem problem = new TruckDeliveryProblem();
-    problem.init(possibleLocations);
-    problem.solve();
-    problem.writeRouteXML(new File(Main.RESOURCE_FOLDER, "routes.xml"));
-    problem.plotSolution(new File(Main.RESOURCE_FOLDER, "solution.png"));
+
+    for (OsmNode node : ProgressBar.wrap(new HashSet<>(possibleLocations),"Filtering unroutable OSM locations")) {
+      if (!node.canBeReachedBy(TruckDeliveryProblem.ENTRY_EDGE,"passenger")) {
+        possibleLocations.remove(node);
+      }
+      if (!node.canRouteTo(TruckDeliveryProblem.EXIT_EDGE,"passenger")) {
+        possibleLocations.remove(node);
+      }
+    }
+
+    {
+      TruckDeliveryProblem problem = new TruckDeliveryProblem();
+      problem.init(possibleLocations, (int) (15000*0.6));
+      problem.solve();
+      problem.writeRouteXML(new File(Main.RESOURCE_FOLDER, "routes_dhl.xml"), "dhl", "#FFFF00");
+      problem.plotSolution(new File(Main.RESOURCE_FOLDER, "solution_dhl.png"));
+    }
+    {
+      TruckDeliveryProblem problem = new TruckDeliveryProblem();
+      problem.init(possibleLocations, (int) (15000*0.25));
+      problem.solve();
+      problem.writeRouteXML(new File(Main.RESOURCE_FOLDER, "routes_ups.xml"), "ups", "#704300");
+      problem.plotSolution(new File(Main.RESOURCE_FOLDER, "solution_ups.png"));
+    }
+    {
+      TruckDeliveryProblem problem = new TruckDeliveryProblem();
+      problem.init(possibleLocations, (int) (15000*0.15));
+      problem.solve();
+      problem.writeRouteXML(new File(Main.RESOURCE_FOLDER, "routes_dpd.xml"), "dpd", "#FF0000");
+      problem.plotSolution(new File(Main.RESOURCE_FOLDER, "solution_dpd.png"));
+    }
+
+
+
     Simulation.close("Done!");
     System.exit(0);
   }
